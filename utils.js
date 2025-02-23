0e9fc9d5d636104470a8e52caa46610f34f90e19
ppattyy <galla355@msu.edu>
2025-02-23 20:25:32 +0000

    Done (finally)

---
+++ utils.js
@@ -0,0 +1,5 @@
+const names = ["Jase", "Jaden", "Carson", "Jeremy", "Frankie", "Xavier", "Tre", "Jesse", "Charlie", "Greg"];
+
+function getRandomName() {
+    return names[Math.floor(Math.random() * names.length)];
+}
